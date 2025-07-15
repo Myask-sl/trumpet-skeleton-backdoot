07f68c85a574cbeafdd864a1bf3b6393d95e79fb
Myask-sl <nospam@invalid.invalid>
2025-07-15 17:41:22 -0600

    Remove swing animation, add back old behavior by default, add fast_doot option

---
+++ src/main/java/com/jamieswhiteshirt/trumpetskeleton/common/Config.java
@@ -0,0 +1,24 @@
+package com.jamieswhiteshirt.trumpetskeleton.common;
+
+import com.jamieswhiteshirt.trumpetskeleton.TrumpetSkeleton;
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+public class Config {
+    public static boolean fast_doot = false;
+    public static void loadConfigs (File eventFile) {
+        File configFile = new File(Paths.get("config") + File.separator + TrumpetSkeleton.MODID + ".cfg");
+        Configuration configuration = new Configuration(configFile);
+
+        fast_doot = configuration.getBoolean(
+            "fast_doot",
+            Configuration.CATEGORY_GENERAL,
+            fast_doot,
+            "Skeletons doot on contact rather than just on timer like original mod."
+        );
+
+        if (configuration.hasChanged()) configuration.save();
+    }
+}
